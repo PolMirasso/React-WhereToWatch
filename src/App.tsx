8191e45b87f1d7cd19c8ebb5aa294e0cc6873266
PolMirasso <pol.mirasso@gmail.com>
2023-03-30 13:54:03 +0200

    fix commits overwrite

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./Router";
 import { Container, Button } from "@mui/material";
 import { NavBar } from "./common/NavBar";
-<<<<<<< Updated upstream
-import FilmList from "./components/UserFilmsList/FilmList";
 import { NotificationProvider } from "./context/notification.context";
 import HomePage from "./pages/home";
-=======
 import FilmList from "./pages/home/FilmList";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <NotificationProvider>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </NotificationProvider>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< Updated upstream
-import { RouterLayout } from './common/RouterLayout';
-import { FilmPage } from './pages/film';
-import { HomePage } from './pages/home';
-import { LoginPage } from './pages/login';
-import { RegisterPage } from './pages/register';
-
-export const AppRouter: React.FC<{}> = () =>{
-    return(
-        <Routes>
-            <Route path='/' element={<RouterLayout/>}>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/film/:id' element={<FilmPage/>}/>
-            </Route>
-            <Route path='/login' element={<LoginPage/>}/>
-            <Route path='/register' element={<RegisterPage/>}/>
-        </Routes>
-    );
-
-}
-=======
 import { RouterLayout } from "./common/RouterLayout";
+import { FilmPage } from "./pages/film";
 import { HomePage } from "./pages/home";
 import { LoginPage } from "./pages/login";
 import { RegisterPage } from "./pages/register";
@@ -31,10 +11,10 @@ export const AppRouter: React.FC<{}> = () => {
     <Routes>
       <Route path="/" element={<RouterLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/film/:id" element={<FilmPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 };
->>>>>>> Stashed changes

fc3b91367b1f6dda69b54b510226d6cb74d58fd1
PolMirasso <pol.mirasso@gmail.com>
2023-04-11 13:51:28 +0200

    navbar beautifuler

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -17,17 +17,22 @@ import FilmList from "../pages/home/FilmList";
 // <Button fullWidth variant='contained'>Register</Button>
 
 export const NavBar = () => {
-
   const [input, setInput] = useState("");
 
   const fetchData = (value: React.SetStateAction<string>) => {
-    fetch("https://wheretowatch-vps.herokuapp.com/getAllFilms/").then ((response) => response.json()).then((json) => {
-      const results = json.filter((film: { name: { toLowerCase: () => React.SetStateAction<string>[]; }; }) => {
-        return film && film.name && film.name.toLowerCase().includes(value)
+    fetch("https://wheretowatch-vps.herokuapp.com/getAllFilms/")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter(
+          (film: {
+            name: { toLowerCase: () => React.SetStateAction<string>[] };
+          }) => {
+            return film && film.name && film.name.toLowerCase().includes(value);
+          }
+        );
+        console.log(results);
       });
-      console.log(results);
-    }); 
-  }
+  };
 
   const handleChange = (value: React.SetStateAction<string>) => {
     setInput(value);
@@ -42,9 +47,15 @@ export const NavBar = () => {
         </a>
 
         <div className="search-box">
-          <input type="search" value={input} onChange={(e) => handleChange(e.target.value)} placeholder="Escriu per cercar..." name="" id="search-input" />
+          <input
+            type="search"
+            value={input}
+            onChange={(e) => handleChange(e.target.value)}
+            placeholder="Escriu per cercar..."
+            name=""
+            id="search-input"
+          />
           <i className="bx">
-            
             <HiOutlineMagnifyingGlass />
           </i>
         </div>
@@ -78,7 +89,9 @@ export const NavBar = () => {
           </a>
         </div>
       </div>
+      {/* <FilmList data={"popular"}></FilmList> */}
       <FilmList></FilmList>
+      {/* <FilmList></FilmList> */}
     </>
   );
 };

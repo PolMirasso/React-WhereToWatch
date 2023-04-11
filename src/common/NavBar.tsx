addfd7292bedf92c23554097d028fd8e2b3ce47d
Tasiees <joantasies@gmail.com>
2023-04-11 13:39:33 +0200

    franco

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { AiOutlineUnorderedList } from "react-icons/ai";
 import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import {
   AppBar,
   Box,
   Toolbar,
@@ -17,17 +23,22 @@ import FilmList from "../pages/home/FilmList";
 // <Button fullWidth variant='contained'>Register</Button>
 
 export const NavBar = () => {
-
   const [input, setInput] = useState("");
 
   const fetchData = (value: React.SetStateAction<string>) => {
-    fetch("https://wheretowatch-vps.herokuapp.com/getAllFilms/").then ((response) => response.json()).then((json) => {
-      const results = json.filter((film: { name: { toLowerCase: () => React.SetStateAction<string>[]; }; }) => {
-        return film && film.name && film.name.toLowerCase().includes(value)
+    fetch("https://wheretowatch-vps.herokuapp.com/getAllFilms/")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter(
+          (film: {
+            name: { toLowerCase: () => React.SetStateAction<string>[] };
+          }) => {
+            return film && film.name && film.name.toLowerCase().includes(value);
+          }
+        );
+        console.log(results);
       });
-      console.log(results);
-    }); 
-  }
+  };
 
   const handleChange = (value: React.SetStateAction<string>) => {
     setInput(value);
@@ -42,20 +53,29 @@ export const NavBar = () => {
         </a>
 
         <div className="search-box">
-          <input type="search" value={input} onChange={(e) => handleChange(e.target.value)} placeholder="Escriu per cercar..." name="" id="search-input" />
+          <input
+            type="search"
+            value={input}
+            onChange={(e) => handleChange(e.target.value)}
+            placeholder="Escriu per cercar..."
+            name=""
+            id="search-input"
+          />
           <i className="bx">
-            
             <HiOutlineMagnifyingGlass />
           </i>
         </div>
-
-        <a href="#" className="user">
+        <li className="dropdown-li">
           <img
             src="https://cdn.discordapp.com/attachments/985160580648292353/1090935434080686090/user.jpg"
             alt=""
             className="user-img"
           />
-        </a>
+          <ul className="dropdown">
+            <label>Benvinguts a Where To Watch</label>
+            <li><a href="/login">LOGIN</a><a href="/register">REGISTER</a></li>
+          </ul>
+        </li>
 
         <div className="navbar">
           <a href="#" className="nav-link nav-active">

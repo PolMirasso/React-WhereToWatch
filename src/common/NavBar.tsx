8c6108e12b6d15befddb0d8067a6140e5d611504
PolMirasso <pol.mirasso@gmail.com>
2023-04-17 12:49:30 +0200

    navbar header

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { AiOutlineUnorderedList } from "react-icons/ai";
 import SearchBar from "./SearchBar";
-import navbar_styles from "../module/navbar.module.css"
+import navbar_styles from "../module/navbar.module.css";
 import { SearchResultsList } from "./SearchResultsList";
 
 export const NavBar = () => {
   const [results, setResults] = useState([]);
 
   return (
-    <>
+    <div className={navbar_styles.header}>
       <div className={`${navbar_styles.nav} ${navbar_styles.container}`}>
         <a href="/" className={navbar_styles.logo}>
           Where<span>To</span>Watch
         </a>
         <div className={navbar_styles.searchBox}>
-        <SearchBar setResults={setResults}/>
+          <SearchBar setResults={setResults} />
         </div>
-        <SearchResultsList results={results}/>
+        <SearchResultsList results={results} />
 
         <ul className={navbar_styles.mainLinks}>
           <li className={navbar_styles.dropdownLi}></li>
-         </ul> 
+        </ul>
         <ul className="main-links">
           <li className="dropdown-li">
             <img
@@ -30,14 +30,25 @@ export const NavBar = () => {
               className={navbar_styles.userImg}
             />
             <ul className={navbar_styles.dropdown}>
-              <li><a className={navbar_styles.log} href="/login">Login</a></li>
-              <li><a className={navbar_styles.reg} href="/register">Register</a></li>
+              <li>
+                <a className={navbar_styles.log} href="/login">
+                  Login
+                </a>
+              </li>
+              <li>
+                <a className={navbar_styles.reg} href="/register">
+                  Register
+                </a>
+              </li>
             </ul>
           </li>
         </ul>
 
         <div className={navbar_styles.navbar}>
-          <a href="#" className={`${navbar_styles.navLink} ${navbar_styles.navActive}`}>
+          <a
+            href="#"
+            className={`${navbar_styles.navLink} ${navbar_styles.navActive}`}
+          >
             <i className={navbar_styles.bx}>
               <AiOutlineUnorderedList />
             </i>
@@ -57,6 +68,6 @@ export const NavBar = () => {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };

cf8df1e0e9cb89f7a3b5836a661feb9448e4b16b
Toni López Llaó <tonilopez450@gmail.com>
2023-04-20 13:31:57 +0200

    Profile style

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,168 +6,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 export const ProfilePage = () => {
+  function follow(): React.MouseEventHandler<HTMLAnchorElement> | undefined {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div
       className={`${navbar_styles.section} ${film_styles.home} ${navbar_styles.container}`}
     >
       {" "}
-      <link
-        rel="stylesheet"
-        href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
-      />
-      <main className="profile-page">
-        <section className="relative block h-500-px">
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80')",
-            }}
-          >
-            <span
-              id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black"
-            ></span>
-          </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0)" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
-        </section>
-        <section className="relative py-16 bg-blueGray-200">
-          <div className="container mx-auto px-4">
-            <div className="relative flex flex-col min-w-0 break-words bg-black w-full mb-6 shadow-xl rounded-lg -mt-64">
-              <div className="px-6">
-                <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        src="https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80"
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        <FontAwesomeIcon icon={faPenToSquare} />
-                      </button>
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-white">
-                          22
-                        </span>
-                        <span className="text-sm text-white">Watched</span>
-                      </div>
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-white">
-                          10
-                        </span>
-                        <span className="text-sm text-white">
-                          Plan to watch
-                        </span>
-                      </div>
-                      <div className="lg:mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-white">
-                          2
-                        </span>
-                        <span className="text-sm text-white">Watching</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-white mb-2">
-                    User Name
-                  </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-white font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-white"></i>
-                    Lorem ipsum,
-                  </div>
-                  <div className="mb-2 text-white mt-10">
-                    <i className="fas fa-briefcase mr-2 text-lg text-white"></i>
-                    Solution Manager - Creative Tim Officer
-                  </div>
-                  <div className="mb-2 text-white">
-                    <i className="fas fa-university mr-2 text-lg text-white"></i>
-                    University of Computer Science
-                  </div>
-                </div>
-                <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-white">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
-                      </p>
-                      <a href="#pablo" className="font-normal text-pink-500">
-                        Show more
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className={profile_styles.frame}>
+        <div className={profile_styles.center}>
+          <div className={profile_styles.profile}>
+            <div className={profile_styles.image}>
+              <div className={profile_styles.circle1}></div>
+              <div className={profile_styles.circle2}></div>
+              <img
+                src="https://wheretowatch-vps.herokuapp.com/static/defaultImageProfile.png"
+                width="70"
+                height="70"
+                alt="Jessica Potter"
+              />
+            </div>
+
+            <div className={profile_styles.name}>Jessica Potter</div>
+            <div className={profile_styles.job}>Visual Artist</div>
+
+            <div className={profile_styles.actions}>
+              <button className={profile_styles.btn}>Follow</button>
+              <button className={profile_styles.btn}>Message</button>
             </div>
           </div>
-          <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-            <div className="container mx-auto px-4">
-              <div className="flex flex-wrap items-center md:justify-between justify-center">
-                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-                  <div className="text-sm text-blueGray-500 font-semibold py-1">
-                    Made with{" "}
-                    <a
-                      href="https://www.creative-tim.com/product/notus-js"
-                      className="text-blueGray-500 hover:text-gray-800"
-                      target="_blank"
-                    >
-                      Notus JS
-                    </a>{" "}
-                    by{" "}
-                    <a
-                      href="https://www.creative-tim.com"
-                      className="text-blueGray-500 hover:text-blueGray-800"
-                      target="_blank"
-                    >
-                      {" "}
-                      Creative Tim
-                    </a>
-                    .
-                  </div>
-                </div>
-              </div>
+
+          <div className={profile_styles.stats}>
+            <div className={profile_styles.box}>
+              <span className={profile_styles.value}>523</span>
+              <span className={profile_styles.parameter}>Posts</span>
             </div>
-          </footer>
-        </section>
-      </main>
+            <div className={profile_styles.box}>
+              <span className={profile_styles.value}>1387</span>
+              <span className={profile_styles.parameter}>Likes</span>
+            </div>
+            <div className={profile_styles.box}>
+              <span className={profile_styles.value}>146</span>
+              <span className={profile_styles.parameter}>Follower</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

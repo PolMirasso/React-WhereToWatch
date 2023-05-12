fd90241b92eb80826c2115164b043d899ba26b98
PolMirasso <pol.mirasso@gmail.com>
2023-05-12 10:30:46 +0200

    register rework

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,6 @@
 import "../../module/tailwind/tailwind.css";
 import React, { useRef, useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import film_styles from "../../module/loginregister.module.css";
 import registerService from "../../services/userManager/register";
 
 type RegisterType = {
@@ -67,162 +66,144 @@ export const RegisterPage: React.FC<{}> = () => {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className={film_styles.logo}>
-            <a href="/" className={`${film_styles.logo} mx-auto h-10 w-auto`}>
-              W<span>T</span>W
-            </a>
+      <div className="min-h-screen flex items-stretch text-white">
+        <div
+          className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center"
+          style={{
+            backgroundImage:
+              "url(https://c4.wallpaperflare.com/wallpaper/621/859/641/peliculas-terror-wallpaper-preview.jpg)",
+          }}
+        >
+          <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+          <div className="w-full px-24 z-10">
+            <h1 className="text-5xl font-bold text-left tracking-wide">
+              Where
+              <span style={{ color: "var(--main-color)" }}>To</span>
+              Watch
+            </h1>
+            <p className="text-3xl my-4">
+              Obtén informació sobre pel·lícules o sèries.
+            </p>
           </div>
-          <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-            Register
-          </h1>
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            onSubmit={handleRegister}
-            encType="multipart/form-data"
+        <div
+          className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0"
+          style={{ backgroundColor: "#161616" }}
+        >
+          <div
+            className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center"
+            style={{
+              backgroundImage:
+                "url(https://c4.wallpaperflare.com/wallpaper/621/859/641/peliculas-terror-wallpaper-preview.jpg)",
+            }}
           >
-            <div>
-              <label
-                htmlFor="text"
-                className="block text-sm font-medium leading-6 text-white"
-              >
-                Nom Usuari
-              </label>
-              <div className="mt-2">
+            <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+          </div>
+          <div className="w-full py-6 z-20">
+            <h1 className="my-6" style={{ fontSize: "5em" }}>
+              <span>Where</span>
+              <span style={{ color: "var(--main-color)" }}>To</span>
+              <span>Watch</span>
+              <br />
+            </h1>
+            <p style={{ fontSize: "3em" }} className="text-gray-100">
+              Register
+            </p>
+            <form
+              className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto"
+              onSubmit={handleRegister}
+              encType="multipart/form-data"
+            >
+              <div className="pb-2 pt-4 ">
                 <input
-                  id="user_name"
-                  name="user_name"
-                  onChange={(event) => setUsername(event.target.value)}
                   type="text"
-                  autoComplete="user_name"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+                  name="user_name"
+                  id="user_name"
+                  placeholder="Nom Usuari"
+                  onChange={(event) => setUsername(event.target.value)}
+                  defaultValue={username}
+                  className="block w-full p-4 text-lg rounded-sm bg-black focus:ring-yellow-500 focus:border-yellow-500"
                 />
               </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-white"
-              >
-                Correu
-              </label>
-              <div className="mt-2">
+              <div className="pb-2 pt-4">
                 <input
+                  type="email"
+                  name="email"
                   id="email"
                   onChange={(event) => setEmail(event.target.value)}
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+                  defaultValue={email}
+                  placeholder="Email"
+                  className="block w-full p-4 text-lg rounded-sm bg-black focus:ring-yellow-500 focus:border-yellow-500"
                 />
               </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Contrasenya
-                </label>
-              </div>
-              <div className="mt-2">
+              <div className="pb-2 pt-4">
                 <input
-                  id="password"
+                  className="block w-full p-4 text-lg rounded-sm bg-black focus:ring-yellow-500 focus:border-yellow-500"
+                  type="password"
                   name="password"
-                  type="password"
+                  id="password"
                   onChange={(event) => setPassword(event.target.value)}
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+                  defaultValue={password}
+                  placeholder="Contrasenya"
                 />
               </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Repetir Contrasenya
+              <div className="pb-2 pt-4">
+                <input
+                  className="block w-full p-4 text-lg rounded-sm bg-black focus:ring-yellow-500 focus:border-yellow-500"
+                  type="password"
+                  name="repeat_password"
+                  id="repeat_password"
+                  onChange={(event) => setRepeatPassword(event.target.value)}
+                  defaultValue={repeatPassword}
+                  placeholder="Repetir Contrasenya"
+                />
+              </div>
+
+              <div className="pb-2 pt-4">
+                <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+                  <span className="flex items-center space-x-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-gray-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      />
+                    </svg>
+                    <span className="font-medium text-gray-600">
+                      {fileName ? fileName : "Drop files to Attach, or"}
+                      <span className="text-blue-600 underline"> browse</span>
+                    </span>
+                  </span>
+                  <input
+                    type="file"
+                    name="image_profile"
+                    className="hidden"
+                    autoComplete="none"
+                    required
+                    onChange={handleFileChange}
+                    defaultValue={image_profile}
+                  />
                 </label>
               </div>
-              <div className="mt-2">
-                <input
-                  id="repeat_password"
-                  name="repeat_password"
-                  type="password"
-                  onChange={(event) => setRepeatPassword(event.target.value)}
-                  autoComplete="none"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
-                />
+
+              {/* <div className="text-right text-gray-400 hover:underline hover:text-gray-100">
+                <a href="#">Forgot your password?</a>
+              </div> */}
+              <span className="text-sm text-red-600">{error}</span>
+              <div className="px-4 pb-2 pt-4">
+                <button className="uppercase block w-full p-4 text-lg rounded-full bg-yellow-500 hover:bg-yellow-600 focus:outline-none">
+                  Registrar
+                </button>
               </div>
-            </div>
-
-            <div className="mt-2">
-              <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
-                <span className="flex items-center space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-gray-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                    />
-                  </svg>
-                  <span className="font-medium text-gray-600">
-                    {fileName ? fileName : "Drop files to Attach, or"}
-                    <span className="text-blue-600 underline"> browse</span>
-                  </span>
-                </span>
-                <input
-                  type="file"
-                  name="image_profile"
-                  className="hidden"
-                  autoComplete="none"
-                  required
-                  onChange={handleFileChange}
-                />
-              </label>
-            </div>
-            <div>
-              {Object.entries(error).map(([key, value]) => (
-                <div key={key}>{value}</div>
-              ))}
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-yellow-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-400"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Ja tens compte?{" "}
-            <a
-              href="/login"
-              className="font-semibold leading-6 text-yellow-400 hover:text-yellow-600"
-            >
-              Inicia Sessio
-            </a>
-          </p>
+            </form>
+          </div>
         </div>
       </div>
     </>
--- src/services/listManager/editListName.tsx
+++ src/services/listManager/editListName.tsx
@@ -5,25 +5,27 @@ const updateListName = async (listNewName, list_id) => {
     const token = await Cookies.get("authToken");
     return token;
   }
-  try {
-    const response = await fetch(
-      "https://wheretowatch-vps.herokuapp.com/updateListName/",
-      {
-        method: "POST",
-        headers: new Headers({
-          Authorization: "Token " + (await getToken()),
-          "Content-Type": "application/x-www-form-urlencoded",
-        }),
-        body: new URLSearchParams({
-          new_name: listNewName,
-          list_id: list_id,
-        }).toString(),
-      }
-    );
-    const data = await response.json();
-    window.location.reload();
-  } catch (error) {
-    console.error("Error adding content list:", error);
+  if (listNewName !== "") {
+    try {
+      const response = await fetch(
+        "https://wheretowatch-vps.herokuapp.com/updateListName/",
+        {
+          method: "POST",
+          headers: new Headers({
+            Authorization: "Token " + (await getToken()),
+            "Content-Type": "application/x-www-form-urlencoded",
+          }),
+          body: new URLSearchParams({
+            new_name: listNewName,
+            list_id: list_id,
+          }).toString(),
+        }
+      );
+      const data = await response.json();
+      window.location.reload();
+    } catch (error) {
+      console.error("Error adding content list:", error);
+    }
   }
 };
 
